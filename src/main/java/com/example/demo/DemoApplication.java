3e3cb7492c8ea5bedc1a32170a7ae6a860819377
Diego Escobar <diego.escobar@ceiba.com.co>
2019-02-07 16:31:17 -0500

    cambio

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.print("try");
+		System.out.print("try1");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
